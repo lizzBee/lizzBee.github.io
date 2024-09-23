74c39aeba94a191e3f5762cf5455b868f50f4d3a
lizzBee <lizz.h@protonmail.com>
2024-09-23 10:33:38 -0400

    npm run silly-business

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Employ me
+          eliazabeth horse knee
         </p>
         <a
           className="App-link"
